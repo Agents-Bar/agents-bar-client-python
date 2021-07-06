83af8855adef082825382f947d29d35314a521e3
Dawid Laszuk <laszukdawid@gmail.com>
2021-07-05 17:53:59 -0700

    Support for environment APIs

--- agents_bar/__init__.py
+++ agents_bar/__init__.py
@@ -2,6 +2,6 @@ from agents_bar.client import Client
 from agents_bar.remote_agent import *
 from agents_bar.utils import *
 
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 __author__ = "Dawid Laszuk"
 
